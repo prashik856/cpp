ebfcf956932c58d63b1fa6583be72689536bdd63
Prashik Raut <prashik856@gmail.com>
2021-08-05 00:37:30 +0530

    Completed printing shortest common supersequence using my code

--- dynamicProgramming/YoutubeCourse/3.LCS/2.Problems/2.PrintingLongestComonSubsequence.py
+++ dynamicProgramming/YoutubeCourse/3.LCS/2.Problems/2.PrintingLongestComonSubsequence.py
@@ -27,3 +27,66 @@ for i in range(n+1):
         table[i].append("")
 print(table)
 
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if(x[i-1] == y[j-1]):
+            table[i][j] =  str(table[i-1][j-1]) + str(x[i-1])
+        else:
+            str1 = table[i-1][j]
+            str2 = table[i][j-1]
+            if(len(str1) > len(str2)):
+                table[i][j] = table[i-1][j]
+            else:
+                table[i][j] = table[i][j-1]
+print(table)
+result = table[n][m]
+print("Required LCS is: ")
+print(result)
+
+# What if we want to reduce the space complexity?
+# We need to use back tracking!
+# first fill the table normally, and then use backtracking
+# How do we do that?
+print("Result using back tracking")
+table = []
+for i in range(n+1):
+    table.append([])
+    for j in range(m+1):
+        table[i].append(-1)
+        if i==0 or j==0:
+            table[i][j]=0
+print(table)
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if(x[i-1] == y[j-1]):
+            table[i][j] = 1 + table[i-1][j-1]
+        else:
+            table[i][j] = max(
+                table[i-1][j],
+                table[i][j-1]
+            )
+print(table)
+# We should use backtracking to get the lcs that we
+# need
+# Now to print the subsequence, we back track from
+# i=n and j=m
+i,j = n,m
+result = ""
+while i>0 and j>0:
+    if(x[i-1] == y[j-1]):
+        result = x[i-1] + result
+        i = i - 1
+        j = j - 1
+    else:
+        value1 = table[i-1][j]
+        value2 = table[i][j-1]
+        if(value1 > value2):
+            i = i - 1
+        else:
+            j = j - 1
+print("Value of result is: ")
+print(result)
+# print("Value of LCS is: ")
+# # this operator will give us the reversed object
+# result = result[::-1]
+# print(result)
---
+++ dynamicProgramming/YoutubeCourse/3.LCS/2.Problems/3.PrintingShortestCommonSuperSequence.py
@@ -0,0 +1,74 @@
+'''
+PRINTING SHORTEST COMMON SUPERSEQUENCE
+
+Problem statement: 
+x : geek
+y : eke
+o/p: gekek
+
+We want to print the short common supersequence
+such that both x and y are the subset of supersequence.
+
+'''
+# Implementing my solution here:
+x = "AGGTAB"
+y = "GXTXAYB"
+n = len(x)
+m = len(y)
+table = []
+for i in range(n + 1):
+    table.append([])
+    for j in range(m+1):
+        table[i].append(-1)
+        if(i==0 or j == 0):
+            table[i][j] = 0
+print(table)
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if(x[i-1] == y[j-1]):
+            table[i][j] = 1 + table[i-1][j-1]
+        else:
+            table[i][j] = max(
+                table[i-1][j],
+                table[i][j-1]
+            )
+print(table)
+
+result = ""
+i = n 
+j = m
+while(i > 0 or j > 0):
+    if i == 0 and j > 0:
+        result = result + y[j-1]
+        print(result)
+        j = j - 1
+        continue
+    
+    if j == 0 and i > 0:
+        result = result + x[i-1]
+        print(result)
+        i = i - 1
+        continue
+        
+    if(x[i-1] == y[j-1]):
+        result = result + x[i-1]
+        print(result)
+        i = i-1
+        j = j-1
+    else:
+        value1 = table[i-1][j]
+        value2 = table[i][j-1]
+        if value1 > value2:
+            result = result + x[i-1]
+            print(result)
+            i = i - 1
+        else:
+            result = result + y[j-1]
+            print(result)
+            j = j - 1
+print("Value of result is: ")
+print(result) 
+print("Value of result is: ")
+result = result[::-1]
+print(result)
