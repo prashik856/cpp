cddb80cee0fc24a630f122b9c9ca4ea6821704ee
Prashik Raut <prashik.raut@oracle.com>
2021-08-24 19:05:45 +0530

    Added leftover files

--- dynamicProgramming/YoutubeCourse/5.DynamicProgrammingOnTrees/1.Learning/2.GeneralSyntax.py
+++ dynamicProgramming/YoutubeCourse/5.DynamicProgrammingOnTrees/1.Learning/2.GeneralSyntax.py
@@ -1,4 +1,47 @@
 '''
 DP on Trees General Syntax.
 
+Tree Recursion
+
+Example function:
+int function(Input) {
+    Base Condition
+
+    Hypothesis -> Giving us the answer of left and right tree
+    (Of children)
+
+    Induction -> Using answers from left and right, we 
+    find the answer for the current node
+    
+    Then the induction will send it's answer to it's parent   
+}
+
+Code:
+
+Return type depends on the question
+Need to pass by reference
+int solve(Node *root, int &result) {
+    BC 
+    if(root == 0) {
+        return 0
+    }
+
+    H
+    int left = solve(root->left, result)
+    int right = solve(root->right, result)
+
+    I
+    int temp = calculate temp answer()
+    int ans = max(temp, someRelation)
+    res = max(res, ans)
+
+    Return step:
+    return temp
+}
+
+We would like to initialize the result with some value
+int res = INT_MIN or INT_MAX
+
+Stored result in res
 '''
+
