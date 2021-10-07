11c44d9befc4d5a9580d9cd5590e78184854f772
Prashik Raut <prashik.raut@oracle.com>
2021-10-07 13:10:08 +0530

    Completed partitions

--- InterviewBit/Arrays/1.ArrayMath/11.Partitions.cpp
+++ InterviewBit/Arrays/1.ArrayMath/11.Partitions.cpp
@@ -60,6 +60,11 @@ Explanation 2:
  1. (0)+(-1,1)+(0).
 */
 
+/**
+ * Solution approach:
+ * https://www.geeksforgeeks.org/count-the-number-of-ways-to-divide-an-array-into-three-contiguous-parts-having-equal-sum/
+ * */
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -75,8 +80,63 @@ int solve(vector<int> &A) {
     int n = A.size();
     int result = 0;
 
+    vector<int> prefix(n,0);
+    vector<int> suffix(n,0);
+
+    // Step1: Create Prefix array
+    int sumValue = 0;
+    for(int i=0; i<A.size(); i++){
+        sumValue += A[i];
+        prefix[i] = sumValue;
+    }
+    // Step 0: if sum of all elements is not divisible by 3, return 0.
+    if(sumValue % 3 != 0) {
+        return 0;
+    }
+
+    int comparisonValue = sumValue/3;
+
+    // Step 2: Create suffix array.
+    vector<int> possibleIndices;
+    sumValue = 0;
+    for(int i=n-1; i>=0; i--){
+        sumValue += A[i];
+        suffix[i] = sumValue;
+    }
+
+    // Step 3: Find all possible indixes in suffix array whose value is sumValue/3
+    for(int i=0; i<suffix.size(); i++){
+        if(suffix[i] == comparisonValue){
+            possibleIndices.push_back(i);
+        }
+    }
     
-    
+    // cout << "Prefix Array: " << endl;
+    // printVector(prefix);
+
+    // cout << "Suffix Array: " << endl;
+    // printVector(suffix);
+
+    // cout << "Possible Index Array: " << endl;
+    // printVector(possibleIndices);
+
+    vector<int>::iterator itr;
+    // Step 4: Traverse the prefix array, find the value equal to sumValue/3
+    for(int i=0; i<n; i++){
+        int value = prefix[i];
+        if(prefix[i] == comparisonValue){
+            // Step 5: If a value is found i, we need to get count of all possible indices
+            // whose value is greater than i + i 
+            // this can be achieved by upperbound
+            // cout << "Index value: " << i << endl;
+            // Use the upperbound function here.
+            itr = upper_bound(possibleIndices.begin(), possibleIndices.end(), i+1);
+            if(itr != possibleIndices.end()) {
+                // cout << "Iterator value: " << (*itr) << endl;
+                result += possibleIndices.end() - itr;
+            }
+        }
+    }
     return result;
 }
 
