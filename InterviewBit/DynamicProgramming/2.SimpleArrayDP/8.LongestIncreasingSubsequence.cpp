ede56e7116dadf6bf5ac43e5192c4eb3a4748381
Prashik Raut <prashik.raut@oracle.com>
2022-05-30 20:15:57 +0530

    Solved some more problems

---
+++ InterviewBit/DynamicProgramming/2.SimpleArrayDP/8.LongestIncreasingSubsequence.cpp
@@ -0,0 +1,368 @@
+/*
+Find the longest increasing subsequence of a given array of integers, A.
+
+In other words, find a subsequence of array in which the subsequence’s elements are in strictly increasing order, and in which the subsequence is as long as possible. 
+
+This subsequence is not necessarily contiguous, or unique.
+
+In this case, we only care about the length of the longest increasing subsequence.
+
+
+
+Input Format:
+
+The first and the only argument is an integer array A.
+Output Format:
+
+Return an integer representing the length of the longest increasing subsequence.
+Constraints:
+
+1 <= length(A) <= 2500
+1 <= A[i] <= 2000
+Example :
+
+Input 1:
+    A = [1, 2, 1, 5]
+
+Output 1:
+    3
+    
+Explanation 1:
+    The sequence : [1, 2, 5]
+
+Input 2:
+    A = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+    
+Output 2:
+    6
+
+Explanation 2:
+    The sequence : [0, 2, 6, 9, 13, 15] or [0, 4, 6, 9, 11, 15] or [0, 4, 6, 9, 13, 15]
+*/
+/*
+Solution approach:
+We already know the Longest Increasing Subsequence code.
+Refer to 1.LengthOfLongestSubsequence.cpp code.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVector(vector<int> arr){
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void print2DVector(vector< vector<int> > arr){
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr[i].size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void print2DVector(vector< vector<bool> > arr){
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr[i].size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void print2DVector(vector< vector<char> > arr){
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr[i].size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+
+void printStrings(vector<string> arr){
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << endl;
+    }
+    cout << endl;
+}
+
+void print2DStrings(vector< vector<string> > arr){
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr[i].size(); j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+class ListNode{
+    public: 
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* createLinkedList(vector<int> arr){
+    ListNode *head = 0;
+    int n = arr.size();
+    for(int i=0; i<n; i++){
+        int value = arr[i];
+        ListNode *newNode = new ListNode(value);
+
+        if(head == 0){
+            head = newNode;
+        } else {
+            ListNode *temp = head;
+            while(temp->next != 0){
+                temp = temp -> next;
+            }
+            temp -> next = newNode;
+        }
+    }
+    return head;
+}
+
+void printLinkedList(ListNode *head){
+    ListNode *temp = head;
+    while(temp != 0){
+        cout << temp->val << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+
+int getLinkedListLength(ListNode *head){
+    ListNode *temp = head;
+    int count = 0;
+    while(temp !=0){
+        temp = temp -> next;
+        count++;
+    }
+    return count;
+}
+
+class TreeNode{
+    public:
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int x): val(x), left(0), right(0){}
+};
+
+TreeNode *createTree(int rootValue, vector<int> a){
+    TreeNode *root = new TreeNode(rootValue);
+
+    // Create Tree
+    for(int i=0; i<a.size(); i++){
+        int val = a[i];
+        TreeNode *node = new TreeNode(val);
+        // We need to find the location for this val
+        TreeNode *temp = root;
+        while(true){
+            // if value is less than current root
+            if(val <= temp -> val){
+                if(temp -> left == 0){
+                    // found it
+                    temp -> left = node;
+                    break;
+                } else {
+                    temp = temp -> left;
+                    continue;
+                }
+            } 
+            // if value is greater than current root
+            else {
+                if(temp -> right == 0){
+                    // found it
+                    temp -> right = node;
+                    break;
+                } else {
+                    temp = temp -> right;
+                    continue;
+                }
+            }
+        }
+    }
+    return root;
+}
+
+// Depth first search
+void preOrder(TreeNode *root){
+    if(root == 0){
+        return;
+    }
+    cout << root -> val << " ";
+    preOrder(root -> left);
+    preOrder(root -> right);
+}
+
+// Sorted order
+void inOrder(TreeNode *root){
+    if(root == 0){
+        return;
+    }
+    inOrder(root -> left);
+    cout << root -> val << " ";
+    inOrder(root -> right);
+}
+
+// Visited all children first
+void postOrder(TreeNode *root){
+    if(root == 0){
+        return;
+    }
+    postOrder(root -> left);
+    postOrder(root -> right);
+    cout << root -> val << " ";
+}
+
+// Breadth first search
+void bfs(TreeNode *root){
+    if(root == 0){
+        return;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode *node = q.front();
+        q.pop();
+        cout << node->val << " ";
+        if(node -> left != 0){
+            q.push(node -> left);
+        }
+        if(node -> right != 0){
+            q.push(node -> right);
+        }
+    }
+}
+
+class DisjointSet {
+    private:
+        vector<int> parent;
+        vector<int> rank;
+        int n;
+    public:
+        DisjointSet(int N){
+            parent.clear();
+            rank.clear();
+            n = N;
+            // parent of i is i
+            for(int i=0; i<n; i++){
+                parent.push_back(i);
+            }
+            // rank is 0?
+            for(int i=0; i<n; i++){
+                rank.push_back(0);
+            }
+        }
+
+        int find(int i){
+            if(parent[i] == i){
+                return i;
+            }
+
+            else {
+                // since parent[i] != i, we find representative of parent[i]
+                int result = find(parent[i]);
+                
+                // Cache value
+                parent[i] = result;
+                
+                return result;
+            }
+        }
+
+        void getUnion(int i, int j) {
+            /*
+            1. If the rank of left is less than the rank of right, then it’s best to move left under right, 
+            because that won’t change the rank of right (while moving right under left would increase the height). 
+            2. In the same way, if the rank of right is less than the rank of left, then we should move right under left.
+            3. If the ranks are equal, it doesn’t matter which tree goes under the other, but the rank of the result will 
+            always be one greater than the rank of the trees.
+            */
+            // get root of i
+            int irep = find(i);
+
+            // get root of j
+            int jrep = find(j);
+
+            // if irep == jrep, we have same root
+            if(irep == jrep){
+                return;
+            }
+
+            // Else, let's get their ranks
+            int rankirep = rank[irep];
+            int rankjrep = rank[jrep];
+
+            if(rankirep == rankjrep){
+                // set anything
+                parent[irep] = jrep;
+                // put i under j
+                // increase rank of j
+                rank[jrep]++;
+            } else if(rankirep > rankjrep) {
+                // we move j under i, and we don't have to increase the rank of i
+                parent[jrep] = irep;
+            } else {
+                // rankjrep > rankirep
+                // we move i under j, and we don't have to increase the rank of j
+                parent[irep] = jrep;
+            }
+        }
+};
+
+// Depth first search using Stack
+void dfs(TreeNode *root){
+    stack<TreeNode *> s;
+    s.push(root);
+    while(!s.empty()){
+        TreeNode *node = s.top();
+        s.pop();
+        cout << node -> val << " ";
+        if(node -> left != 0){
+            s.push(node -> left);
+        }
+        if(node -> right != 0){
+            s.push(node -> right);
+        }
+    }
+}
+
+int solve(vector<int> &A) {
+    // minimum value of result
+    int result = 1;
+
+    vector<int> lis(A.size(), 1);
+
+    for(int i=1; i<A.size(); i++) {
+        for(int j=0; j<i; j++){
+            if(A[i] > A[j] && lis[i] <= lis[j]){
+                lis[i] = lis[j] + 1;
+                result = max(result, lis[i]);
+            }
+        }
+    }
+    return result;
+}
+
+int main(){
+    vector<int> a = {84, 80, 27};
+    cout << "Given array is: " << endl;
+    printVector(a);
+
+    int result = solve(a);
+    cout << "Result: " << result << endl;
+    return 0;
+}
+
+// g++ -std=c++11
