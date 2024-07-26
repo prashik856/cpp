f2fc08f7e18bb9e7d93b3bae33a7edeb8a4fd4ea
Prashik Raut <prashik856@gmail.com>
2024-07-26 14:00:37 +0530

    completed maximum subarray sum

--- slidingWindow/2.MaximumSubarraySum.cpp
+++ slidingWindow/2.MaximumSubarraySum.cpp
@@ -1,4 +1,11 @@
-/**
+/** Maximum Sum Subarray of Size K
+ * 
+ * Flow: 
+ * 1. Problem Statement Input Output
+ * 2. Identification
+ * 3. Abstract - Code
+ * 4. Code
+ * 
  * Given an array: 2,3,5,2,9,7,1
  * Input n = 3.
  * 
@@ -12,6 +19,13 @@
  * 
  * Improvements can be done. Can we remove some repetivie work?
  * 
+ * If i is first index, and j is the second index,
+ * window size can be represented as (j - i + 1)
+ * 
+ * Once (j-i+1) == k, we can then slide the window.
+ * Till we hit this point, we can keep on increasing the value of j while keeping i constant.
+ * 
+ * 
  */
 #include<bits/stdc++.h>
 using namespace std;
@@ -21,6 +35,28 @@ void printVector(vector<int> arr){
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+int maxSubarraySumWindow(vector<int>&arr, int n) {
+    int output = 0;
+    int sum = 0;
+    int i=0,j=0;
+    while(j<arr.size()) {
+        sum = sum + arr[j];
+        if(j-i+1 < n) {
+            j++;
+        } else if(j-i+1 == n) {
+            if(sum > output) {
+                output = sum;
+            }
+
+            sum = sum - arr[i];
+            i++;
+            j++;
+        }
+    }
+
+    return output;
 }
 
 int maxSubarraySum(vector<int>&arr, int n) {
@@ -57,7 +93,7 @@ int maxSubarraySum(vector<int>&arr, int n) {
 }
 
 int main() {
-    vector<int> arr = vector<int>({2, 3, 5, 2, 9, 7, 1});
+    vector<int> arr = vector<int>({2, 3, 5, 2, 9, 7, 8});
     int n = 3;
     cout << "Input Array: " << endl;
     printVector(arr);
@@ -65,4 +101,7 @@ int main() {
 
     int output = maxSubarraySum(arr, n);
     cout << "Output: " << output << endl;
+
+    output = maxSubarraySumWindow(arr, n);
+    cout << "Output Window: " << output << endl;
 }
