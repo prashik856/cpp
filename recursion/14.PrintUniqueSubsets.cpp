b000c563d7ea9ed533ec07fc67b3430f18620938
Prashik Raut <prashik856@gmail.com>
2024-08-06 18:41:48 +0530

    completed unique subsets

--- recursion/14.PrintUniqueSubsets.cpp
+++ recursion/14.PrintUniqueSubsets.cpp
@@ -1,7 +1,27 @@
 /**
  * Print Unique Subsets and Variations
  * 
- * Solutino should not have duplicates and every subset should be unique.
+ * Solution should not have duplicates and every subset should be unique.
+ * 
+ * What are the variations of this problem?
+ * 
+ * Print All Subsets Or Print PowerSet or Print All Subsequences
+ * They all eventually mean the same thing.
+ * 
+ * PowerSet is basically all subsets of a set.
+ * 
+ * Substring - Substring is continuous.
+ * 
+ * Print All Subsequences - Subsequence can be discontinuous. But Order matters in subsequence.
+ * If we have an array, abc, ac is a subsequence, but ca is not a subsequeuce.
+ * So Order is important here.
+ * 
+ * Subset - subset can be discontinuous. Order does not matter here.
+ * So, for the above example, ac is allowed and ca is also allowed.
+ * 
+ * Now, Substring is a subset of Subsequence. Subsequence is a subset of Powerset.
+ * Powerset contains all possible combinations.
+ * 
  */
 #include<bits/stdc++.h>
 using namespace std;
