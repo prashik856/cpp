5cc5f486587fd1b72315d6b02508de1b1319732c
Prashik Raut <prashik856@gmail.com>
2024-08-14 21:32:17 +0530

    Completed minimum difference element in sorted array

--- binarysearch/15.MinimumDifferenceElementInSortedArray.cpp
+++ binarysearch/15.MinimumDifferenceElementInSortedArray.cpp
@@ -11,10 +11,13 @@
  * Output:
  * 3
  * 
- * What we can do is, we can first find ceil and then find floor of the key.
- * And using these two values, we can return the element which has minimum difference.
- * While finding this, if our input[mid] value equals key, return that input[mid] value
- * as that is output.
+ * 
+ * Simple binary search does this for us.
+ * Rather than finding the floor and Ceil, we can simply run binary search
+ * if we find key, return that index.
+ * Else, go till high < low.
+ * once that happens, we got two indices which is closes to key.
+ * Find the difference between two, and return the index with lowest difference.
  */
 #include<bits/stdc++.h>
 using namespace std;
@@ -24,6 +27,37 @@ void printVector(vector<int> arr){
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+int findMinOptimal(vector<int> &input, int key) {
+    int output = -1;
+
+    int low =0;
+    int high = input.size() - 1;
+    while(high >= low) {
+        int mid = (high + low)/2;
+
+        if(input[mid] == key) {
+            return mid;
+            break;
+        }
+
+        if(key > input[mid]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    int lowDiff = abs(input[low] - key);
+    int highDiff = abs(input[high] - key);
+    if(lowDiff < highDiff) {
+        output = low;
+    } else {
+        output = high;
+    }
+
+    return output;
 }
 
 int findMinDifference(vector<int> &input, int key) {
@@ -57,14 +91,18 @@ int findMinDifference(vector<int> &input, int key) {
         // All values are greater than key
         // the smallest element is closest to key
         floor = 0;
-    }
-    
-    if(floor + 1 < input.size()) {
-        // this value will be closes to key
+        
+        // since all values are greater than key,
+        // the smallest element will be closest to key
+        ceil = 0;
+    } else if(floor + 1 < input.size()) {
+        // floor != -1
+
+        // this value will be closest to key
         ceil = floor + 1;
-    } 
-    
-    if(floor + 1 >= input.size()) {
+    } else if(floor + 1 >= input.size()) {
+        // floor != -1
+        
         // no ceil value available
         // no value which is greater than or equal to key
         // All values are smaller than key.
@@ -90,6 +128,12 @@ int main() {
     cout << "Element: " << key << endl;
 
     int output = findMinDifference(input, key);
+    cout << "Output: " << output << endl;
+    if(output >= 0) {
+        cout << "Value: " << input[output] << endl;
+    }
+
+    output = findMinOptimal(input, key);
     cout << "Output: " << output << endl;
     if(output >= 0) {
         cout << "Value: " << input[output] << endl;
