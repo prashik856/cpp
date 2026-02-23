2746a6c0be99361eccc054bc7c536ff25f1d20d0
Prashik Raut <prashik856@gmail.com>
2026-02-23 14:53:15 +0000

    Commpleted 3 su mclosest

--- leetcode/16.3SumClosest.cpp
+++ leetcode/16.3SumClosest.cpp
@@ -21,6 +21,101 @@ Constraints:
 
     3 <= nums.length <= 500
     -1000 <= nums[i] <= 1000
-    -104 <= target <= 104
+    -10^4 <= target <= 1^04
 
  */
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVector(vector<int> &arr) {
+    for (int i=0; i<arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int getClosestValue(vector<int> &nums, int target, int i, int j , int k) {
+    int result = -1;
+    int current_max = INT_MAX;
+    while (j < k) {
+        int currentSum = nums[i] + nums[j] + nums[k];
+        // cout << "Current Sum: " << currentSum << endl;
+
+        int diff = abs(target - currentSum);
+
+        if (diff < current_max) {
+            current_max = diff;
+            result = currentSum;
+        }
+
+        // Equal value
+        if (currentSum == target) {
+            result = currentSum;
+            break;
+        }
+
+        if (currentSum < target) {
+            j++;
+        }
+
+        if (currentSum > target) {
+            k--;
+        }
+    }
+
+    return result;
+}
+
+int threeSumClosest(vector<int>& nums, int target) {
+    int result = 0;
+
+    sort(nums.begin(), nums.end());
+    // cout << "Sorted arrary is: " << endl;
+    // printVector(nums);
+
+    int n = nums.size();
+
+    int currentMax = INT_MAX;
+    for (int i=0; i<n-2; i++) {
+
+        // cout << "Index: " << i << " value: " << nums[i] << endl;
+        int tempResult = getClosestValue(nums, target, i, i+1, n-1);
+
+
+        if (tempResult == target) {
+            result = target;
+            break;
+        }
+
+        int diff = abs(target - tempResult);
+
+        if (diff < currentMax) {
+            currentMax = diff;
+            result = tempResult;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    vector<vector<int>> inputs = {{-1,2,1,-4}, {0,0,0}, {1,1,1,1}, {4,0,5,-5,3,3,0,-4,-5}, {-100,-98,-2,-1}, {2,3,8,9,10}, {-1000,-5,-5,-5,-5,-5,-5,-1,-1,-1}};
+    vector<int> targets = {1, 1, 3, -2, -101, 16, -14};
+    vector<int> outputs = {2, 0, 3, -2, -101, 15, -15};
+
+    for (int i=0; i<inputs.size(); i++) {
+        cout << "Input is: " << endl;
+        printVector(inputs[i]);
+
+        cout << "Target is: " << targets[i] << endl;
+
+        int output = threeSumClosest(inputs[i], targets[i]);
+        cout << "Output: " << output << endl;
+        cout << "Expected output: " << outputs[i] << endl << endl;
+    }
+    return 0;
+}
+
+// Remember, here we can have duplicates.
+// And the end result is similar to 3 sum with duplicates
+// And check the absolute value after getting the result from the helper function
